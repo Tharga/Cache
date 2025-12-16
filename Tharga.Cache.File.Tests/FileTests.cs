105d1b80d6a2966004228b1fbfcd941be98454be
Daniel Bohlin <daniel.bohlin@thargelion.se>
2025-12-16 02:03:43 +0100

    handle file access

---
+++ Tharga.Cache.File.Tests/FileTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace Tharga.Cache.File.Tests;
+
+public class FileTests
+{
+    [Fact(Skip = "Just run when required.")]
+    public async Task WriteToFile()
+    {
+        //Arrange
+        var parts = new[]
+        {
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Tharga", "Cache", "Test",
+            $"{Guid.NewGuid()}.txt"
+        };
+        var path = Path.Combine(parts);
+
+        var sut = new FileService();
+
+        //Act
+        await sut.SetDataAsync(path, "A");
+
+        //Arrange
+        await sut.DeleteDataAsync(path);
+    }
+
+    [Fact(Skip = "Just run when required.")]
+    public async Task ReadAndWriteToFile()
+    {
+        //Arrange
+        var parts = new[]
+        {
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Tharga", "Cache", "Test",
+            $"{Guid.NewGuid()}.txt"
+        };
+        var path = Path.Combine(parts);
+
+        var sut = new FileService();
+        var a = sut.SetDataAsync(path, new string('A', 100_000_000));
+        var b = sut.GetDataAsync(path);
+
+        //Act
+        await Task.WhenAll(a, b);
+
+        //Arrange
+        await sut.DeleteDataAsync(path);
+    }
+}
